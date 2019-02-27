c0e4fbadfbdf0618a9eec16079acdb9fe2143fc9
Jessica Dussault <jdussault4@gmail.com>
2019-02-27 12:56:54 -0600

    fighting with titles and text
    adds titleize
    temporarily adding gemfile but it is invalid
    because I'm working from datura locally
    as it has not yet been merged and released

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "datura", git: "https://github.com/CDRH/datura.git", tag: "v0.1.0"
+
+gem 'titleize', '~> 1.4', '>= 1.4.1'
--- scripts/overrides/tei_to_es_poem.rb
+++ scripts/overrides/tei_to_es_poem.rb
@@ -1,3 +1,5 @@
+require "titleize"
+
 class TeiToEsPoem < TeiToEs
 
   # NOTE: please refer to tei_to_es overrides in this directory for most
@@ -11,27 +13,56 @@ class TeiToEsPoem < TeiToEs
     # puts @json["identifier"]
   end
 
+  def get_first_line
+    # grab the first 7 words in the fisrt line of the poem, since there
+    # are no poem titles in this edition, or return nil if line is not found
+    line = @xml.at_xpath(".//l")
+    line.text.split(" ")[0..6].join(" ") if line
+  end
+
   def keywords
     # TODO adding in order to have way to discern types of content for LoG
     [ "poem" ]
   end
 
   def title
-    if @id == "ppp.00271"
-      # grab the first 7 words in the fisrt line of the poem, since there
-      # are no poem titles in this edition
-      line = @xml.at_xpath(".//l")
-      if line
-        opening = line.text.split(" ")[0..6].join(" ")
-        "Leaves of Grass, \"#{opening}\", #{@year}"
-      else
-        "Leaves of Grass, Untitled Poem, #{@year}"
-      end
+
+    # majority of editions simply grab identified poem title
+    label = @xml.at_xpath(@xpaths["title"]["main"])
+    if label
+      label = label.text
+      label.gsub!(/[0-9]+ — /, "")
     else
-      # majority of editions simply grab identified poem title
-      label = get_text(@xpaths["title"]["main"])
-      "#{label} (#{@year})" if @year
+      label = ""
     end
+
+    # for two editions, will need to do something fancier for title
+    if @id == "ppp.00271"
+      # need to get the opening lines for 1855 edition
+      opening = get_first_line
+      if opening
+        label = "Leaves of Grass, \"#{opening}\""
+      else
+        label = "Leaves of Grass, Untitled Poem"
+      end
+
+    elsif @id == "ppp.01500" || @id == "ppp.00473"
+      # need to get the poem cluster's title if poem doesn't have title
+      if label[/^\d+\.?/] || label == ""
+        cluster = @xml.at_xpath("./parent::lg[@type='cluster']//head[@type='main-authorial']")
+        if label == ""
+          # if there is no label at all, we need to try to get the index of this particular poem
+          siblings = @xml.xpath("./preceding-sibling::lg[@type='poem']")
+          label = siblings.length + 1
+        end
+        label = "#{cluster.text} #{label}" if cluster
+      end
+      label.gsub!(".", "")
+
+    end
+
+    label = label.titleize
+    @year ? "#{label} (#{@year})" : label
   end
 
   def uri
