aa083c37ae7c0e9df190d620c3c97e34fe9f82ea
William Dewey <wkdewey@gmail.com>
2022-09-20 10:13:40 -0500

    add overrides for clusters

--- scripts/overrides/file_tei.rb
+++ scripts/overrides/file_tei.rb
@@ -13,6 +13,7 @@ class FileTei < FileType
       "//div1[@type='essay' or @type='preface']" => TeiToEsOther,
       "//titlePart[@type='imprimatur']" => TeiToEsOther,
       "//div3[@type='letter' or @type='article']" => TeiToEsOther,
+      "//lg[@type='cluster']" => TeiToEsCluster,
       "//lg[@type='poem' and @id and contains(@id, 'ppp')]" => TeiToEsPoem
     }
   end
