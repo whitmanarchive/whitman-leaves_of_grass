f73b87c6ed295cbc59874caa2999e4614be70a02
William Dewey <wkdewey@gmail.com>
2022-09-22 15:42:33 -0500

    add has_part, is_part_of, and citation to clusters

--- scripts/overrides/tei_to_es_cluster.rb
+++ scripts/overrides/tei_to_es_cluster.rb
@@ -20,4 +20,45 @@ class TeiToEsCluster < TeiToEs
     label = "Cluster: #{label.downcase.titleize}"
     @year ? "#{label} (#{@year})" : label
   end
+
+  def has_part
+    poems = @xml.xpath("//lg[@type='poem' and @id and contains(@id, 'ppp')]")
+    parts = []
+    poems.each do |poem_xml|
+      poem = TeiToEsPoem.new(poem_xml, {}, nil, @filename)
+      parts << {
+        "role" => "contained poem",
+        "id" => poem.get_id,
+        "title" => poem.title
+      }
+    end
+    parts
+  end
+
+  def is_part_of
+    title = @xml.at_xpath("./ancestor::TEI//titleStmt/title[1]")
+    {
+      "role" => "containing work",
+      "id" => @filename,
+      "title" => title.text
+    }
+  end
+
+  def citation
+    # WorksInfo is get_works_info.rb in whitman-scripts repo
+    @works_info = WorksInfo.new(xml)
+    ids, names = @works_info.get_works_info
+    citations = []
+    if ids.length > 0
+      ids.each_with_index do |id, idx|
+        name = names[idx]
+        citations << {
+          "id" => id,
+          "title" => name,
+          "role" => "whitman_id"
+        }
+      end
+    end
+    citations
+  end
 end
