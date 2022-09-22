8f405b3a8746eedfff38e0f0a6c3c1fa475f265c
William Dewey <wkdewey@gmail.com>
2022-09-22 15:39:14 -0500

    add citation, next and previous items, and is_part_of to poems

--- scripts/overrides/tei_to_es_poem.rb
+++ scripts/overrides/tei_to_es_poem.rb
@@ -6,7 +6,14 @@ class TeiToEsPoem < TeiToEs
   # of the behavior related to the poem content of leaves of grass
 
   # TODO might be better to change the behavior in datura
+  def preprocessing
+    @cluster = @xml.at_xpath("./parent::lg[@type='cluster']")
+  end
+
   def get_id
+    if !@xml["id"]
+      return nil
+    end
     poem_id = @xml["id"].gsub("ppp.", "poem.")
     "#{@filename}_#{poem_id}"
   end
@@ -69,4 +76,68 @@ class TeiToEsPoem < TeiToEs
     "poem"
   end
 
+  def is_part_of
+    title = @xml.at_xpath("./ancestor::TEI//titleStmt/title[1]")
+    parts = [{
+      "role" => "containing work",
+      "id" => @filename,
+      "title" => title.text
+    }]
+    if @cluster
+      cluster_id = @cluster["id"].gsub("ppp.", "cluster.")
+      cluster_title = @cluster.at_xpath(".#{@xpaths["title_main"]}")
+      parts << {
+        "role" => "containing cluster",
+        "id" => "#{@filename}_#{cluster_id}",
+        "title" => cluster_title.text
+      }
+    end
+    parts
+  end
+
+  def previous_item
+    prev_node = @xml.at_xpath("./preceding::lg[@type='poem'][1]")
+    if prev_node
+      poem = TeiToEsPoem.new(prev_node, {}, nil, @filename)
+      prev_id = poem.get_id
+      prev_title = poem.title
+      {
+        "role" => "previous poem",
+        "id" => prev_id,
+        "title" => prev_title
+      }
+    end
+  end
+
+  def next_item
+    next_node = @xml.at_xpath("./following::lg[@type='poem'][1]")
+    if next_node
+      poem = TeiToEsPoem.new(next_node, {}, nil, @filename)
+      next_id = poem.get_id
+      next_title = poem.title
+      {
+        "role" => "next poem",
+        "id" => next_id,
+        "title" => next_title
+      }
+    end
+  end
+
+  def citation
+    # WorksInfo is get_works_info.rb in whitman-scripts repo
+    @works_info = WorksInfo.new(xml)
+    ids, names = @works_info.get_works_info
+    citations = []
+    if ids.length > 0
+      ids.each_with_index do |id, idx|
+        name = names[idx]
+        citations << {
+          "id" => id,
+          "title" => name,
+          "role" => "whitman_id"
+        }
+      end
+    end
+    citations
+  end
 end
