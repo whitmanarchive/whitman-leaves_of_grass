063391fda9fa189e571cd0458ab441271a73d56e
Jessica Dussault <jdussault4@gmail.com>
2022-10-25 18:04:00 -0500

    tinkers with classes for poem and book, checks for dupes

--- scripts/poem_identifier.rb
+++ scripts/poem_identifier.rb
@@ -2,21 +2,83 @@
 
 require "nokogiri"
 
-source_files = Dir[File.join(__dir__, "../source/tei/**/*.xml")]
+@poem_all_ids = []
+@poem_duplicate_ids = []
 
-source_files.each do |file|
-  xml = File.open(file) { |f| Nokogiri::XML(f) }
-  # removes custom Whitman TEI namespace, will use id instead of xml:id
-  xml.remove_namespaces!
+def main
+  source_files = Dir[File.join(__dir__, "../source/tei/**/*.xml")]
 
-  poems = xml.xpath("//lg[@type='poem']")
-  poems_with_ids = xml.xpath("//lg[@type='poem' and boolean(@id)]")
+  source_files.each do |file|
+    book = BookOfPoems.new(file)
 
-  puts "Reading poem: #{File.basename(file)}"
-  puts "    Has #{poems.length}"
-  puts "    Missing ids for #{poems.length - poems_with_ids.length}"
+    # ideally move this into the book class, but I'm too lazy to
+    # work out sharing arrays across them
+    book.poems.each do |poem|
+      next if poem.id.empty?
+      if @poem_all_ids.include?(poem.id)
+        @poem_duplicate_ids << poem.id
+      else
+        @poem_all_ids << poem.id
+      end
+    end
+    book.report
+  end
+
+  if @poem_duplicate_ids.length > 0
+    puts "duplicate ids: #{@poem_duplicate_ids.uniq}"
+  end
+
+  # uncomment to see all the current identifiers
+  # puts @poem_all_ids.sort
+  puts "Final identifier: #{@poem_all_ids.sort.last}"
 end
 
+class BookOfPoems
+  def initialize(path)
+    @path = path
+    @xml = File.open(path) { |f| Nokogiri::XML(f) }
+    # removes custom Whitman TEI namespace, will use id instead of xml:id
+    @xml.remove_namespaces!
+  end
 
-# add existing ids to a list we can check for duplicates in
+  def find_and_create_poems
+    lgs = @xml.xpath("//lg[@type='poem' or @type='cluster']")
+    lgs.map do |lg|
+      Poem.new(lg)
+    end
+  end
 
+  def poems
+    @poems ||= find_and_create_poems
+  end
+
+  def poems_with_id
+    @poems_with_id ||= poems.select { |p| p.has_id? }
+  end
+
+  def poems_without_id
+    @poems_without_id ||= poems.reject { |p| p.has_id? }
+  end
+
+  def report
+    puts "Reading file: #{File.basename(@path)}"
+    puts "    Has #{poems.length}"
+    puts "    Missing ids for #{poems_without_id.length}"
+  end
+end
+
+class Poem
+  def initialize(element)
+    @element = element
+  end
+
+  def id
+    @id ||= @element.xpath("@id").text
+  end
+
+  def has_id?
+    !id.empty?
+  end
+end
+
+main
