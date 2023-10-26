8329204a9ad1ad6e5b906948b606d354416815ac
William Dewey <wkdewey@gmail.com>
2023-10-26 16:33:33 +0000

    add 'other' items to volume contained parts

--- scripts/overrides/tei_to_es.rb
+++ scripts/overrides/tei_to_es.rb
@@ -156,6 +156,10 @@ class TeiToEs < XmlToEs
   def has_part
     poems = @xml.xpath("//lg[@type='poem' and @id and contains(@id, 'ppp')]")
     clusters = @xml.xpath("//lg[@type='cluster']")
+    # getting all the xpaths that go to TeiToEsOther
+    other1 = @xml.xpath("//div1[@type='essay' or @type='preface']")
+    other2 = @xml.xpath("/titlePart[@type='imprimatur']")
+    other3 = @xml.xpath("//div3[@type='letter' or @type='article']")
     parts = []
     poems.each do |poem_xml|
       poem = TeiToEsPoem.new(poem_xml, {}, nil, @filename)
@@ -171,6 +175,30 @@ class TeiToEs < XmlToEs
         "role" => "contained cluster",
         "id" => cluster.get_id,
         "title" => cluster.title
+      }
+    end
+    other1.each do |other_xml|
+      other = TeiToEsOther.new(other_xml, {}, nil, @filename)
+      parts << {
+        "role" => "contained other section",
+        "id" => other.get_id,
+        "title" => other.title
+      }
+    end
+    other2.each do |other_xml|
+      other = TeiToEsOther.new(other_xml, {}, nil, @filename)
+      parts << {
+        "role" => "contained other section",
+        "id" => other.get_id,
+        "title" => other.title
+      }
+    end
+    other3.each do |other_xml|
+      other = TeiToEsOther.new(other_xml, {}, nil, @filename)
+      parts << {
+        "role" => "contained other section",
+        "id" => other.get_id,
+        "title" => other.title
       }
     end
     parts
